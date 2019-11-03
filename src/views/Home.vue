f225435d15eb190d7ade484a7aa4fc2d35ca86b8
DmitriyTro <Dmitriytro@gmail.com>
2019-11-03 13:36:00 -0800

    add: initialize project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Twelvetro"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import GameField from '@/components/GameField.vue';
 
 export default {
-  name: 'home',
+  name: 'Game field',
   components: {
-    HelloWorld,
+    GameField,
   },
 };
 </script>
