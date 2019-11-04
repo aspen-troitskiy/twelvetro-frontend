ea3027d58af06ac3b4c4b0745f9855b7bb2af8f3
DmitriyTro <Dmitriytro@gmail.com>
2019-11-03 17:41:45 -0800

    add: styling and alignment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Base</router-link>
+      <router-link to="/">Twelvetro</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,10 +15,8 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -27,5 +25,19 @@
       color: #42b983;
     }
   }
+}
+#wrapper{
+  width: 650px  ;
+  height: auto;
+  background-color: rgb(198, 241, 200);
+  margin: 0 auto;
+  margin-top: 200px;
+  border-radius: 10px;
+}
+html, 
+body {
+  padding: 0;
+  margin: 0;
+  background-color: #121212;
 }
 </style>
--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <div class="hex-row">
-      <div v-for="(hex, index) in fieldData[0]" :key="`hex-${index}`">
-        <Hex :hex="hex"></Hex>
+  <div class="content">
+    <div class="game-field">
+      <div class="hex-row small-row">
+        <div v-for="(hex, index) in fieldData[0]" :key="`hex-${index}`">
+          <Hex :hex="hex"></Hex>
+        </div>
       </div>
-    </div>
-    <div class="hex-row even">
-      <div v-for="(hex, index) in fieldData[1]" :key="`hex-${index}`">
-        <Hex :hex="hex"></Hex>
+      <div class="hex-row even">
+        <div v-for="(hex, index) in fieldData[1]" :key="`hex-${index}`">
+          <Hex :hex="hex"></Hex>
+        </div>
       </div>
-    </div>
-    <div class="hex-row">
-      <div v-for="(hex, index) in fieldData[2]" :key="`hex-${index}`">
-        <Hex :hex="hex"></Hex>
+      <div class="hex-row">
+        <div v-for="(hex, index) in fieldData[2]" :key="`hex-${index}`">
+          <Hex :hex="hex"></Hex>
+        </div>
       </div>
-    </div>
-    <div class="hex-row even">
-      <div v-for="(hex, index) in fieldData[3]" :key="`hex-${index}`">
-        <Hex :hex="hex"></Hex>
+      <div class="hex-row even">
+        <div v-for="(hex, index) in fieldData[3]" :key="`hex-${index}`">
+          <Hex :hex="hex"></Hex>
+        </div>
       </div>
-    </div>
-    <div class="hex-row">
-      <div v-for="(hex, index) in fieldData[4]" :key="`hex-${index}`">
-        <Hex :hex="hex"></Hex>
+      <div class="hex-row small-row">
+        <div v-for="(hex, index) in fieldData[4]" :key="`hex-${index}`">
+          <Hex :hex="hex"></Hex>
+        </div>
       </div>
     </div>
   </div>
@@ -109,12 +111,25 @@ export default {
     }
   },
 };
+
 </script>
 <style scoped lang="scss">
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .game-field {
+    height: 496px;
+    width: 538px;
+  }
   .hex-row {
-      clear: left;   
+    clear: left;   
   }
   .hex-row.even {
-      margin-left: 53px;
+    margin-left: 53px;
+  }
+  .hex-row.small-row {
+    margin-left: 106px;
   }
 </style>
